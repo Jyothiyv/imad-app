3ca06c7c0c445c38a7c8c0d71c2e5ca686b45f0b
Jyothi YV <30960395+Jyothiyv@users.noreply.github.com>
2017-08-19 02:28:43 -0600

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/Article-One', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'Article-One.html'));
 });
 app.get('/Article-Two', function (req, res) {
   res.send('Article two requested here and will be served here.')});
